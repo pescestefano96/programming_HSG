12d1ab9cfa909f11285fde9c840235df9a172ab7
Ualh <105986401+Ualh@users.noreply.github.com>
2022-06-04 11:52:37 +0200

    Update v1.py

--- v1.py
+++ v1.py
@@ -12,8 +12,9 @@ yf.pdr_override()
 
 #Choose between 3 markets
 
-x = input('Tap sp500 or nasdaq or dow')
-if (x == 'sp500'):
+x = input('Tap: ')
+
+if (x == 'a'):
     tickers = si.tickers_sp500()
     tickers = [item.replace(".", "-") for item in tickers] # Yahoo Finance uses dashes instead of dots
     index_name =  '^GSPC' # S&P 500
@@ -21,7 +22,7 @@ if (x == 'sp500'):
     end_date = datetime.date.today()
     exportList = pd.DataFrame(columns=['Stock', "RS_Rating", "50 Day MA", "150 Day Ma", "200 Day MA", "52 Week Low", "52 week High"])
     returns_multiples = []
-if (x == 'nasdaq'):
+if (x == 'b'):
     tickers = si.tickers_nasdaq()
     tickers = [item.replace(".", "-") for item in tickers] # Yahoo Finance uses dashes instead of dots
     index_name =  '^IXIC' # S&P 500
@@ -29,13 +30,11 @@ if (x == 'nasdaq'):
     end_date = datetime.date.today()
     exportList = pd.DataFrame(columns=['Stock', "RS_Rating", "50 Day MA", "150 Day Ma", "200 Day MA", "52 Week Low", "52 week High"])
     returns_multiples = []
-if (x == 'dow'):
+if (x == 'c'):
     tickers = si.tickers_dow()
     tickers = [item.replace(".", "-") for item in tickers] # Yahoo Finance uses dashes instead of dots
     index_name =  '^DJI' # S&P 500
     start_date = datetime.datetime.now() - datetime.timedelta(days=365)
     end_date = datetime.date.today()
     exportList = pd.DataFrame(columns=['Stock', "RS_Rating", "50 Day MA", "150 Day Ma", "200 Day MA", "52 Week Low", "52 week High"])
-    returns_multiples = []    
-else:
-    print('error, please choose either sp500, nasdaq or dow !')
+    returns_multiples = []
